71a5cb74d995929cf90a407648a6eab2cb68918f
AlberLC <alberlc@outlook.com>
2024-07-04 03:02:00 +0200

    Fix heating_context (models.__init__)

--- flanabot/models/__init__.py
+++ flanabot/models/__init__.py
@@ -1,6 +1,7 @@
 from flanabot.models.bot_action import *
 from flanabot.models.chat import *
 from flanabot.models.enums import *
+from flanabot.models.heating_context import *
 from flanabot.models.message import *
 from flanabot.models.player import *
 from flanabot.models.punishment import *
